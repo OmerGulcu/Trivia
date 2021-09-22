e2ee7cc502b702dce9b71f45ca4354d467173826
Ömer Gülcü <gulcuomer@gmail.com>
2021-09-22 12:27:56 +0300

    Added question 3

--- script1.js
+++ script1.js
@@ -1,14 +1,22 @@
 let i1 = 0;
-function checkOption(button,answer) {
-    if (i1 < 1) {
-        i1++;
+let i3 = 0;
+
+function checkOption(button,answer,question) {
+    if (question == 1 && i1 < 1 || question == 3 && i3 < 1) {
         if (answer == "correct") {
             button.style.backgroundColor = "green";
-            document.querySelector("#feedback1").innerHTML = "Correct!";
+            document.querySelector(`#feedback${question}`).innerHTML = "Correct!";
         }
         else {
             button.style.backgroundColor = "red";
-            document.querySelector("#feedback1").innerHTML = "Incorrect.";
+            document.querySelector(`#feedback${question}`).innerHTML = "Incorrect.";
+        }
+
+        if (question == 1) {
+            i1++;
+        }
+        else {
+            i3++;
         }
     }
 };
