95be4bae4e9eda511f55e7494429839267b4f7ff
Ömer Gülcü <gulcuomer@gmail.com>
2021-09-23 13:49:18 +0300

    Separated pc and quiz

---
+++ pc/pccheckbutton.js
@@ -0,0 +1,20 @@
+let counters = [];
+
+for (let i = 0; i < 50; i++) {
+    counters.push(0);
+}
+
+function checkOption(button,answer,question) {
+    if (counters[question - 1] < 1) {
+        if (answer == "correct") {
+            button.style.backgroundColor = "green";
+            document.querySelector(`#feedback${question}`).innerHTML = "Correct!";
+        }
+        else {
+            button.style.backgroundColor = "red";
+            document.querySelector(`#feedback${question}`).innerHTML = "Incorrect.";
+        }
+        
+        counters[question - 1] = 1;
+    }
+};
